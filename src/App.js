00596de8b4befc8f375f0363e97f46f0dc7bd9e9
Tom Mahle <tmahle@tempworks.com>
2020-02-29 16:24:00 -0600

    clone a repo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import git from 'isomorphic-git';
-import FS from '@isomorphic-git/lightning-fs';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import git from "isomorphic-git";
+import http from "isomorphic-git/http/web";
+import FS from "@isomorphic-git/lightning-fs";
 
 function App() {
-    const dir = '/tutorial';
-    console.log(git, FS);
+  console.log(git, FS);
+  const fs = new FS("fs");
+  git
+    .clone({
+      fs,
+      http,
+      dir: "/tutorial",
+      corsProxy: "https://cors.isomorphic-git.org",
+      url: "https://github.com/isomorphic-git/isomorphic-git",
+      singleBranch: true,
+      depth: 1
+    })
+    .then(x => console.log("success"))
+    .catch(x => console.log(x));
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
